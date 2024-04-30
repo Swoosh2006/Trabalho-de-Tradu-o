71cd538bf019ceb4425e3caacd6e23dbf694048c
Swoosh2006 <166552466+Swoosh2006@users.noreply.github.com>
2024-04-30 16:10:18 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+dicionario={"blessed" : "abençoado", "give" : "dar", "read" : "ler", "say" : "dizer", "asked" : "perguntou", "going" : "vai", "keep" : "manter", "began" : "começou", "need" : "necessitar", "got" : "obteve", "being" : "sendo", "see" : "ver", "made" : "feito", "found" : "encontrado", "take" : "pegar", "called" : "chamou", "looked" : "olhou", "must" : "deve", "come" : "vir", "use" : "usar", "did" : "fez", "will" : "vai"}
+
+for English, Portugues in dicionario. items ():
+    print (f'ingles : {English} Portugues: {Portugues}')
